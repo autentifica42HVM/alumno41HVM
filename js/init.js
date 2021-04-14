25cabfbb930b2c87f419348140135c2c0259f67d
autentifica42HVM <81263778+autentifica42HVM@users.noreply.github.com>
2021-04-13 21:45:19 -0500

    Create init.js

---
+++ js/init.js
@@ -0,0 +1,18 @@
+/*  Inicializa Firebase con la
+ * configuración del proyecto.
+ * Revisa la configuración en tu
+ * servidor de Firebase y cópiala
+ * aquí sustituyendo los
+ * asteriscos. Los campos deben
+ * quedar igual que en tu
+ * servidor. */
+// @ts-ignore
+firebase.initializeApp({
+    apiKey: "AIzaSyBA1YSiX9KjXAhZ9a0o5941EXDz-pKTZo4",
+    authDomain: "alumno42hvm.firebaseapp.com",
+    projectId: "alumno42hvm",
+    storageBucket: "alumno42hvm.appspot.com",
+    messagingSenderId: "206753348767",
+    appId: "1:206753348767:web:d1bb7515201b36cc3df6aa",
+    measurementId: "G-ZXVF8JXLGD"
+});
